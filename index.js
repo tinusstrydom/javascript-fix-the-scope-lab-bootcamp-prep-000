44b3b0f9a83a0deff7df7109c8ee947bd76dc5cf
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-08 16:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ function yourAnimal() {
   return animal
 }
 
-
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
+  // Feel free to move things around!
 }
 
 var funkyFunction = function() {
